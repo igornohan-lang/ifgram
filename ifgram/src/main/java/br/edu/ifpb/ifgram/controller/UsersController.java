52c3fab3e9f46926895efdc99768ec09ed42135c
Igor Nohan Fernandes Soares <202412060013@ad.ifpb.edu.br>
2026-09-10 08:13:59 -0300

    endpoint

---
+++ ifgram/src/main/java/br/edu/ifpb/ifgram/controller/UsersController.java
@@ -0,0 +1,33 @@
+package br.edu.ifpb.ifgram.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping ("users")
+public class UsersController {
+
+        @GetMapping
+        public String getUser(){
+            return "get user was called";
+        }
+
+        @PostMapping
+    public String postUser(){
+            return "chamei o endpoint como um POST";
+        }
+
+        @DeleteMapping
+    public String deleteUser(){
+            return "chamei o endpoint como um DELETE";
+        }
+
+        @PutMapping
+    public String putUser(){
+            return "chamei o endpoint como um PUT";
+        }
+
+        @PatchMapping
+    public String patchUser(){
+            return "chamei o endpoint como um PATCH";
+        }
+}
